a2642dd215429256d51c4bc684f90b497fa162a9
JunHatanaka <nextseason6@gmail.com>
2022-03-22 00:49:04 +0900

    Modify: prettier実行

--- pages/ramen/index.tsx
+++ pages/ramen/index.tsx
@@ -1,55 +1,66 @@
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-    const [nowFullTime, setNowFullTime] = useState("")
+  const [nowFullTime, setNowFullTime] = useState('')
 
-    function zeroPadding(num: number,length: number){
-        return ('0' + num).slice(-length);
-    }
+  function zeroPadding(num: number, length: number) {
+    return ('0' + num).slice(-length)
+  }
 
-    async function showClock(){
-        const nowTime = new Date();
-        const nowHour = nowTime.getHours();
-        const nowMin  = nowTime.getMinutes();
-        const nowSec  = nowTime.getSeconds();
-        const msg =  zeroPadding(nowHour, 2) + ":" + zeroPadding(nowMin, 2) + ":" + zeroPadding(nowSec, 2);
-        setNowFullTime(msg);
-    }
+  async function showClock() {
+    const nowTime = new Date()
+    const nowHour = nowTime.getHours()
+    const nowMin = nowTime.getMinutes()
+    const nowSec = nowTime.getSeconds()
+    const msg =
+      zeroPadding(nowHour, 2) +
+      ':' +
+      zeroPadding(nowMin, 2) +
+      ':' +
+      zeroPadding(nowSec, 2)
+    setNowFullTime(msg)
+  }
 
-    useEffect(()=>{
-        setInterval(()=>{
-            showClock()
-        },1000);
-    },[]);
-    
-    
-    // var array = []
-    // function setTime(){
-    //     let com = document.getElementById('comment-field').value;
-    //     localStorage.setItem('comment', com)
-    //     localStorage.setItem('time', document.getElementById('RealtimeClockArea').textContent)
-    // };
-    
-    // function showTimes(){
-    //     document.getElementById('show-comment-field').textContent = localStorage.getItem('comment');
-    //     document.getElementById('show-time-field').textContent = localStorage.getItem('time');
-    // };
-    // setInterval('showTimes()',1000);
+  useEffect(() => {
+    setInterval(() => {
+      showClock()
+    }, 1000)
+  }, [])
+
+  // var array = []
+  // function setTime(){
+  //     let com = document.getElementById('comment-field').value;
+  //     localStorage.setItem('comment', com)
+  //     localStorage.setItem('time', document.getElementById('RealtimeClockArea').textContent)
+  // };
+
+  // function showTimes(){
+  //     document.getElementById('show-comment-field').textContent = localStorage.getItem('comment');
+  //     document.getElementById('show-time-field').textContent = localStorage.getItem('time');
+  // };
+  // setInterval('showTimes()',1000);
   return (
     <>
-        <title>カップラーメンTimer</title>
-        <div className="container">
-        <div className="clock" id="RealtimeClockArea">{nowFullTime}</div>
-        <div style={{textAlign: "center"}}>
-            <input type="text" id="comment-field" value="調理開始" placeholder="コメントを入力してください"></input>
-            <input type="button" value="ボタン"></input>
+      <title>カップラーメンTimer</title>
+      <div className="container">
+        <div className="clock" id="RealtimeClockArea">
+          {nowFullTime}
         </div>
-            <div style={{textAlign: "center"}}>
-                <span id="show-comment-field"></span>
-                <span id="show-time-field"></span>
-            </div>
+        <div style={{ textAlign: 'center' }}>
+          <input
+            type="text"
+            id="comment-field"
+            value="調理開始"
+            placeholder="コメントを入力してください"
+          ></input>
+          <input type="button" value="ボタン"></input>
         </div>
+        <div style={{ textAlign: 'center' }}>
+          <span id="show-comment-field"></span>
+          <span id="show-time-field"></span>
+        </div>
+      </div>
     </>
   )
 }
