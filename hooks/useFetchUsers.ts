5afd4db61db377c3493724a112854a641c4c82b4
JunHatanaka <nextseason6@gmail.com>
2022-04-07 00:19:03 +0900

    Add: hooks/useFetchUsers.tsの処理を完成

--- hooks/useFetchUsers.ts
+++ hooks/useFetchUsers.ts
@@ -4,4 +4,6 @@ import { useState } from 'react'
 export const useFetchUsers = () => {
   const [userList, setUserList] = useState([{ id: 1 }])
   const onClickFetchUser = () => alert('関数実行')
+
+  return { userList, onClickFetchUser }
 }
