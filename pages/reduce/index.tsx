9471d6a7a069d32b430a553ee1d232ab2ab6f1d8
JunHatanaka <nextseason6@gmail.com>
2022-04-09 02:07:14 +0900

    Add: reduceを動かす

---
+++ pages/reduce/index.tsx
@@ -0,0 +1,29 @@
+// https://zenn.dev/nekoniki/articles/07c09eb6811c85a753de
+
+export const App = () => {
+    type Student = {
+        name: string;
+        score: number;
+    }
+
+    const data: Student[] = [
+        { name: '太郎', score: 75 },
+        { name: '花子', score: 62 },
+        { name: 'John', score: 59 }
+    ]
+
+    const totalScore: number = data.reduce((acc: number, val: Student): number => {
+        // accは「初期値 or 前回のreturn値」でvalは「配列要素」
+        return acc + val.score;
+    }, 0);
+
+    console.log(totalScore);
+
+  return (
+    <div>
+        {totalScore}
+     </div>
+  )
+}
+
+export default App
