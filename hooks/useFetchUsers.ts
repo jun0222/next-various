32b368e81152073b7081d36e4facd538082b5efd
JunHatanaka <nextseason6@gmail.com>
2022-04-08 10:05:31 +0900

    Modify: hooks/useFetchUsers.tsの処理を変更 途中まで

--- hooks/useFetchUsers.ts
+++ hooks/useFetchUsers.ts
@@ -1,9 +1,15 @@
 // モダンJavaScriptの基礎から始めるReact実践の教科書 p.249
+import axios from 'axios'
 import { useState } from 'react'
 
 export const useFetchUsers = () => {
-  const [userList, setUserList] = useState([{ id: 1 }])
-  const onClickFetchUser = () => alert('関数実行')
+  const [userList, setUserList] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const onClickFetchUser = () => {
+    setLoading(true)
+    setError(false)
+  }
 
   return { userList, onClickFetchUser }
 }
