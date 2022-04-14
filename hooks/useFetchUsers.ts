cb42c691676a78c4d9255d735692f7c648f6e620
JunHatanaka <nextseason6@gmail.com>
2022-04-15 01:10:21 +0900

    Add: カスタムhooksのハンズオンまで完了

--- hooks/useFetchUsers.ts
+++ hooks/useFetchUsers.ts
@@ -3,7 +3,13 @@ import axios from 'axios'
 import { useState } from 'react'
 
 export const useFetchUsers = () => {
-  const [userList, setUserList] = useState([])
+  type User = {
+    id: number
+    name: string
+    age: number
+  }
+
+  const [userList, setUserList] = useState<User[]>([]) // https://chaika.hatenablog.com/entry/2020/08/16/083000
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
   const onClickFetchUser = () => {
@@ -12,22 +18,22 @@ export const useFetchUsers = () => {
   }
 
   axios
-  .get('https://jsonplaceholder.typicode.com/users')
-  .then(result => {
-    const users = result.data.map(user => {
-      id: user.id,
-      name: user.name,
-      age: user.age
-    })
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((result) => {
+      const users = result.data.map((user: User) => ({
+        id: user.id,
+        name: user.name,
+        age: user.age,
+      }))
 
-    setUserList(users)
-  })
-  .catch(error => {
-    setError(true)
-  })
-  .finally(() => {
-    setLoading(false)
-  })
+      setUserList(users)
+    })
+    .catch((error) => {
+      setError(true)
+    })
+    .finally(() => {
+      setLoading(false)
+    })
 
   return { userList, loading, error, onClickFetchUser }
 }
--- pages/customHooks/index.tsx
+++ pages/customHooks/index.tsx
@@ -1,20 +1,16 @@
-// モダンJavaScriptの基本から始めるReact実践の教科書 p.245
+// モダンJavaScriptの基本から始めるReact実践の教科書 p.245~
 
-import { useState } from 'react'
 import { useFetchUsers } from '../../hooks/useFetchUsers'
 
 export const App = () => {
   const { userList, loading, error, onClickFetchUser } = useFetchUsers()
 
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
-
   return (
     <div>
       <button onClick={onClickFetchUser}>Fetch User</button>
 
-      {isError && <p>Error!</p>}
-      {isLoading ? (
+      {error && <p>Error!</p>}
+      {loading ? (
         <p>Loading...</p>
       ) : (
         userList.map((user) => <p key={user.id}>{`${user.id}${user.name}`}</p>)
