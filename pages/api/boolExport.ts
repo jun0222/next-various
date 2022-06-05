02e8abd7073081b3a6fe90233a6bcfa7e5d3f9ad
JunHatanaka <nextseason6@gmail.com>
2022-06-06 01:39:04 +0900

    Add: JavaScriptのimport循環でundefinedになる件（再現できず）

---
+++ pages/api/boolExport.ts
@@ -0,0 +1,7 @@
+// 参考:https://qiita.com/taharah/items/ef69f2b722844cc249f6
+// 参考:https://tech.dely.jp/entry/ten_trivia_of_typescript_#1-%E5%BE%AA%E7%92%B0%E4%BE%9D%E5%AD%98%E3%81%AE%E3%82%A8%E3%83%A9%E3%83%BC%E3%82%92%E5%9B%9E%E9%81%BF%E3%81%99%E3%82%8B%E6%96%B9%E6%B3%95
+import { usuallyValue } from './boolImport'
+
+export const boolValue = 'aaa' === 'aaa'
+
+export const usuallyValueIncrement = usuallyValue + 1
---
+++ pages/api/boolImport.ts
@@ -0,0 +1,10 @@
+import { boolValue } from './boolExport'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const usuallyValue = boolValue + '1' // ここが違う。もっと依存させる？
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  const getBool = boolValue ? 'true' : 'false'
+  console.log(boolValue)
+  res.status(200).json({ bool: getBool })
+}
