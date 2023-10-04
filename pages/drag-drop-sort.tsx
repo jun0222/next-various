5b3fb2ea9edfd7afe06c8f4b7ade222f77287fac
JunHatanaka <nextseason6@gmail.com>
2023-10-05 02:16:43 +0900

    feature: react-beautiful-dndのハック方法リンク記載

--- pages/drag-drop-sort.tsx
+++ pages/drag-drop-sort.tsx
@@ -1,5 +1,6 @@
 // 参考: https://qiita.com/kalbeekatz/items/bd9c3e8aff6117fdae42
 // reactStrictModeを外さないとエラーが出る？ https://github.com/atlassian/react-beautiful-dnd/issues/2407
+// https://medium.com/@wbern/getting-react-18s-strict-mode-to-work-with-react-beautiful-dnd-47bc909348e4で対応？
 import DragDropSort from '@/components/DragDropSort'
 
 const DragDropSortPage = () => {
