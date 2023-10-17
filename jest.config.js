65124aa5de20c90150b46ff693061482b96831d3
jun0222 <47178202+jun0222@users.noreply.github.com>
2023-10-18 02:25:19 +0900

    fix: jestのwarning対応 (#10)
    * feature: mswサンプル
    
    msw
    
    * fix: jestのwarning対応

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   roots: ['<rootDir>'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.test.json', // TODO: テスト用のtsconfigを作成し、compilerOptionsをreact設定する。アプリ側はpreserve。
+        diagnostics: false,
+      },
+    ],
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json', // TODO: テスト用のtsconfigを作成し、compilerOptionsをreact設定する。アプリ側はpreserve。
-      diagnostics: false,
-    },
-  },
 }
