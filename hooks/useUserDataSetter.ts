9fc8685b6ee162efca3f8c5bbe3844dca23b250e
JunHatanaka <nextseason6@gmail.com>
2024-02-26 14:37:56 +0900

    feature: jsdoc

--- hooks/useUserDataSetter.ts
+++ hooks/useUserDataSetter.ts
@@ -1,6 +1,17 @@
 import { useUser } from '../contexts/UserContext'
 import { useCallback } from 'react'
 
+/**
+ * カスタムフック `useUserDataSetter` を使用して、ユーザー情報を取得し、
+ * UserContextにセットする。このフックは、ユーザー情報と、
+ * その情報を取得してセットする関数を返す。
+ *
+ * `useCallback`を使用する理由:
+ * `fetchAndSetUser` 関数は、`setUser` ステートセッター関数に依存している。
+ * `useCallback`を用いることで、`setUser` 関数が変更されない限り、
+ * `fetchAndSetUser`関数の参照をメモ化し、不必要な再レンダリングや
+ * `useEffect` の依存配列を使用している場合の無限ループを防ぐ。
+ */
 export const useUserDataSetter = () => {
   const { user, setUser } = useUser()
 
